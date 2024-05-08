3968de2fbc2b32c22cdc5224d70b292262ecc9a1
thurein <thureintun@acedatasystems.com>
2024-05-08 22:45:38 +0630

    added search box

--- app/src/main/java/com/thurein/moviesdb/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/thurein/moviesdb/core/presentation/HomeScreen.kt
@@ -2,12 +2,26 @@ package com.thurein.moviesdb.core.presentation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.thurein.moviesdb.presentation.MovieListScreen
@@ -21,7 +35,10 @@ fun HomeScreen(navController: NavHostController) {
     val movieState = movieListViewModel.movieListState.collectAsState().value
 
     Scaffold(
-        
+        topBar = {
+            SearchBar(modifier = Modifier) // Add margins here
+
+        }
     ) {
         Box(modifier = Modifier
             .fillMaxSize()
@@ -35,5 +52,46 @@ fun HomeScreen(navController: NavHostController) {
 
         }
     }
+}
+@Composable
+fun SearchBar(
+    modifier: Modifier=Modifier
+) {
+    val searchTextState = remember { mutableStateOf(TextFieldValue()) }
+
+    OutlinedTextField(
+        value = searchTextState.value,
+        onValueChange = {
+            searchTextState.value = it
+            // onSearch(it.text)
+        },
+        modifier  = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+
+        placeholder = {
+            Text(
+                text = "Search a movie or tv series",
+                color = MaterialTheme.colorScheme.primaryContainer,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                // onSearch(searchTextState.value.text)
+            }
+        ),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon"
+            )
+        },
+
+        )
 }
 
