0b022cdd374c14da408f348169ba8b3f201f1200
thurein <thureintun@acedatasystems.com>
2024-05-08 14:30:56 +0630

    testing search bar

--- app/src/main/java/com/thurein/moviesdb/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/thurein/moviesdb/core/presentation/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -19,7 +20,7 @@ fun HomeScreen(navController: NavHostController) {
     val movieListViewModel = hiltViewModel<MovieListViewModel>()
     val movieState = movieListViewModel.movieListState.collectAsState().value
 
-    Scaffold {
+    Scaffold() {
         Box(modifier = Modifier
             .fillMaxSize()
             .padding(it)) {
